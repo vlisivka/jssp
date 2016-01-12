952a59dfe34f844eb492c7141e9a9caedb03f244
vlisivka <vlisivka@gmail.com>
2016-01-12 18:18:01 +0200

    Servlet is runnable now.

--- src/main/java/org/jssp/Main.java
+++ src/main/java/org/jssp/Main.java
@@ -1,13 +1,21 @@
 package org.jssp;
 
-public class Main {
+import java.io.IOException;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class Main extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
     }
-
 }
